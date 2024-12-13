60a9fbaab98457086a6fea9aab7a445ba44bb3a0
rushikeshnarule <75885428+rushikeshnarule@users.noreply.github.com>
2024-12-14 00:47:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -81,7 +81,7 @@ BUTTONS2 = {}
 #         except Exception as e:
 #             logger.error(f"Chat Not verified : {e}") 
 
-@Client.on_message(filters.group & filters.text & filters.incoming)
+@Client.on_message(filters.group | filters.private & filters.text & filters.incoming)
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
